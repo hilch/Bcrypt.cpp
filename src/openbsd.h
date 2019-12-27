9106ae80d2196e3217b0749ae137c167bf8dbf6c
Christoph Hilchenbach <christoph@hilchenba.ch>
2019-12-27 16:52:10 +0100

    Move functions in openbsd.cpp to openbsd.h

--- src/openbsd.h
+++ src/openbsd.h
@@ -2,10 +2,29 @@
 #define ARC4RANDOM_H_INCLUDED
 
 #include <cstddef>
+#include <cstring>
+#include <ctime>
+
+inline
+void arc4random_buf(void *buf, size_t nbytes)
+{
+    for( size_t n = 0; n < nbytes; ++ n)
+        ((char*)(buf))[n] = rand() %256;
+}
+
+inline
+void arc4random_init(void)
+{
+    srand( (unsigned int) time(NULL));
+}
+
+inline
+void    explicit_bzero(void *b, size_t len)
+{
+    volatile void * dont_optimize = memset( b, 0, len );
+    dont_optimize = dont_optimize;
+}
 
 
-void     arc4random_buf(void *buf, size_t nbytes);
-void    explicit_bzero(void *b, size_t len);
-void arc4random_init(void);
 
 #endif // ARC4RANDOM_H_INCLUDED
